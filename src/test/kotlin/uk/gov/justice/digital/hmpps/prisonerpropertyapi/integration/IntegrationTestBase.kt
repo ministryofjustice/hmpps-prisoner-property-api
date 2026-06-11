9f74f4bf2220e808dca6c1801418268024b44610
Michael Willis <Michael.Willis2@justice.gov.uk>
2026-06-11 15:50:22 +0100

    MAPB-452 Provide postgres and localstack for integration tests
    - Switch CI verify job to gradle_localstack_postgres_verify (starts postgres + localstack
      services), matching hmpps-locations-inside-prison-api
    - Add detect-or-start PostgresContainer and LocalStackContainer test helpers so tests also
      run locally without the CI services
    - Wire both into IntegrationTestBase via @DynamicPropertySource
    - Point application-test datasource at the provided postgres service

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpropertyapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpropertyapi/integration/IntegrationTestBase.kt
@@ -7,7 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDO
 import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient
 import org.springframework.http.HttpHeaders
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
+import uk.gov.justice.digital.hmpps.prisonerpropertyapi.config.LocalStackContainer
+import uk.gov.justice.digital.hmpps.prisonerpropertyapi.config.LocalStackContainer.setLocalStackProperties
+import uk.gov.justice.digital.hmpps.prisonerpropertyapi.config.PostgresContainer
 import uk.gov.justice.digital.hmpps.prisonerpropertyapi.integration.wiremock.HmppsAuthApiExtension
 import uk.gov.justice.digital.hmpps.prisonerpropertyapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
 import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
@@ -23,6 +28,25 @@ abstract class IntegrationTestBase {
 
   @Autowired
   protected lateinit var jwtAuthHelper: JwtAuthorisationHelper
+
+  companion object {
+    private val pgContainer = PostgresContainer.instance
+    private val localStackContainer = LocalStackContainer.instance
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun properties(registry: DynamicPropertyRegistry) {
+      pgContainer?.run {
+        registry.add("spring.datasource.url", ::getJdbcUrl)
+        registry.add("spring.datasource.username", ::getUsername)
+        registry.add("spring.datasource.password", ::getPassword)
+        registry.add("spring.flyway.url", ::getJdbcUrl)
+        registry.add("spring.flyway.user", ::getUsername)
+        registry.add("spring.flyway.password", ::getPassword)
+      }
+      localStackContainer?.also { setLocalStackProperties(it, registry) }
+    }
+  }
 
   internal fun setAuthorisation(
     username: String? = "AUTH_ADM",
